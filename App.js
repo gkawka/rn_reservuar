41f6afa88595302b7316359c634edea970db150e
Andrzej Markowski <andrzej@kreatzje.pl>
2018-03-24 14:36:19 +0100

    ui

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
-import {
-    Container,
-    Header,
-    Body,
-    Icon,
-    Title,
-} from 'native-base';
+import { Image } from 'react-native';
+import { Button, Card, CardItem, Container, FlatList, Header, ListView, Body, Icon, Text, Title, Left, Right } from 'native-base';
 
 export default class HeaderExample extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { loading: true };
+        this.state = {
+            loading: true,
+            rooms: []
+        };
     }
 
     async componentWillMount() {
@@ -24,19 +22,62 @@ export default class HeaderExample extends Component {
         this.setState({ loading: false });
     }
 
+    componentDidMount(){
+        return fetch('http://172.17.5.66:3000/songs')
+            .then((response) => response.json())
+            .then((json) => {
+                rooms = json;
+                this.setState({
+                  loading: false,
+                  rooms: json
+                }, function() {});
+            }).catch((error) =>{
+              console.error(error);
+            });
+    }
+
     render() {
         if (this.state.loading) {
             return <Expo.AppLoading />;
-        }
+        } else {
+            return (
+                <Container>
+                    <Header>
+                        <Left>
+                            <Button transparent>
+                                <Image source={{ uri: 'https://png.icons8.com/ios/1600/sausage.png' }} style={{ width: 40, height: 40 }} />
+                            </Button>
+                        </Left>
 
-        return (
-            <Container>
-                <Header>
-                    <Body>
-                        <Title>Salki</Title>
-                    </Body>
-                </Header>
-            </Container>
-        );
+                        <Body>
+                            <Title>Salami</Title>
+                        </Body>
+
+                        <Right/>
+                    </Header>
+
+                    <Card style={{ flex: 0 }}>
+                        <CardItem header>
+                            <Icon name='home' />
+                            <Text>Zielona</Text>
+                        </CardItem>
+
+                        <CardItem>
+                            <Body>
+                                <Text>
+                                    Opis salki
+                                </Text>
+                            </Body>
+                        </CardItem>
+
+                        <CardItem footer>
+                            <Button style={{ backgroundColor: '#3fa800' }}>
+                                <Text>Rezerwuj</Text>
+                            </Button>
+                        </CardItem>
+                     </Card>
+                </Container>
+            );
+        }
     }
 }
