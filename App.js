4329aa275cf32451ece2381f0b8ddf963247056e
krzysiekmajewski <majewski.krzysiek@gmail.com>
2018-03-24 16:24:04 +0100

    Interfejs graficzny

--- App.js
+++ App.js
@@ -1,16 +1,31 @@
-import React, { Component } from 'react';
-import { Image } from 'react-native';
-import { Button, Card, CardItem, Container, FlatList, Header, ListView, Body, Icon, Text, Title, Left, Right } from 'native-base';
+import React, {Component} from 'react';
+import {Image} from 'react-native';
+import {
+    Button,
+    Card,
+    CardItem,
+    Container,
+    FlatList,
+    Header,
+    ListView,
+    Body,
+    Icon,
+    Text,
+    Title,
+    Left,
+    Right,
+    Content
+} from 'native-base';
 
 export default class HeaderExample extends Component {
     constructor(props) {
         super(props);
-
-        this.state = {
-            loading: true,
-            rooms: []
-        };
     }
+
+    state = {
+        isReady: false,
+        rooms: []
+    };
 
     async componentWillMount() {
         await Expo.Font.loadAsync({
@@ -19,65 +34,75 @@ export default class HeaderExample extends Component {
             Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
         });
 
-        this.setState({ loading: false });
+        this.setState({isReady: true});
     }
 
-    componentDidMount(){
-        return fetch('http://172.17.5.66:3000/songs')
+    componentDidMount() {
+        return fetch('https://jsonplaceholder.typicode.com/users')
             .then((response) => response.json())
             .then((json) => {
                 rooms = json;
                 this.setState({
-                  loading: false,
-                  rooms: json
-                }, function() {});
-            }).catch((error) =>{
-              console.error(error);
+                    rooms: json
+                }, function () {
+                });
+            }).catch((error) => {
+                console.error(error);
             });
     }
 
     render() {
-        if (this.state.loading) {
-            return <Expo.AppLoading />;
-        } else {
-            return (
-                <Container>
-                    <Header>
-                        <Left>
-                            <Button transparent>
-                                <Image source={{ uri: 'https://png.icons8.com/ios/1600/sausage.png' }} style={{ width: 40, height: 40 }} />
-                            </Button>
-                        </Left>
 
-                        <Body>
-                            <Title>Salami</Title>
-                        </Body>
-
-                        <Right/>
-                    </Header>
-
-                    <Card style={{ flex: 0 }}>
-                        <CardItem header>
-                            <Icon name='home' />
-                            <Text>Zielona</Text>
-                        </CardItem>
-
-                        <CardItem>
-                            <Body>
-                                <Text>
-                                    Opis salki
-                                </Text>
-                            </Body>
-                        </CardItem>
-
-                        <CardItem footer>
-                            <Button style={{ backgroundColor: '#3fa800' }}>
-                                <Text>Rezerwuj</Text>
-                            </Button>
-                        </CardItem>
-                     </Card>
-                </Container>
-            );
+        if (this.state.isReady === false) {
+            return <Expo.AppLoading/>;
         }
+
+        let cards = [];
+        this.state.rooms.forEach((item) => {
+            cards.push(
+                <Card key={item.id} style={{flex: 0}}>
+                    <CardItem header>
+                        <Icon name='home'/>
+                        <Text>{item.username}</Text>
+                    </CardItem>
+
+                    <CardItem>
+                        <Body>
+                        <Text>
+                            {item.name}
+                        </Text>
+                        </Body>
+                    </CardItem>
+
+                    <CardItem footer>
+                        <Button>
+                            <Text>Rezerwuj</Text>
+                        </Button>
+                    </CardItem>
+                </Card>
+            );
+        });
+
+        return (
+            <Container>
+                <Header>
+                    <Left>
+                        <Button transparent>
+                            <Image source={{uri: 'https://png.icons8.com/ios/1600/sausage.png'}}
+                                   style={{width: 40, height: 40}}/>
+                        </Button>
+                    </Left>
+
+                    <Body>
+                    <Title>Salami</Title>
+                    </Body>
+
+                </Header>
+
+                <Content>
+                    {cards}
+                </Content>
+            </Container>
+        );
     }
 }
